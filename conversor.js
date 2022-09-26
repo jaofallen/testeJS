e8a973792b60857dbdc1f9adf7f77c4b4927508d
jaofallen <joao2304@gmail.com>
2022-09-26 20:02:27 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,63 +1,67 @@
-let valueMeters = prompt("Insira o valor em Metros: ");
+let valueMeters = prompt("Insira Resultado em Metros: ");
 
 // let valueMetersNumeric = parseFloat(valueMeters);
 
 const measurementChoice = prompt(
-  "Para qual medida deseja converter? \n\nmilímetro (mm)\ncentímetro (cm)\ndecímetro (dm)\ndecâmetro (dam)\nhectômetro (hm)\nquilômetro (km)"
+  "Para qual medida deseja converter? \n\n1) milímetro (mm)\n2) centímetro (cm)\n3) decímetro (dm)\n4) decâmetro (dam)\n5) hectômetro (hm)\n6) quilômetro (km)"
 );
 
 let resultChoice;
 
-// switch (measurementChoice) {
-//   case "mm":
-//     valueMeters * 1000;
-//     alert("O valor é: " + valueMeters * 1000 + " milimetros");
-//     break;
-//   case "cm":
-//     valueMeters * 100;
-//     alert("O valor é: " + valueMeters * 100 + " centimetros");
-//     break;
-//   case "dm":
-//     valueMeters * 10;
-//     alert("O valor é: " + valueMeters * 10 + " decimetros");
-//     break;
-//   case "dam":
-//     valueMeters / 10;
-//     alert("O valor é: " + valueMeters / 10 + " decametros");
-//     break;
-//   case "hm":
-//     valueMeters / 100;
-//     alert("O valor é: " + valueMeters / 100 + " hectometros");
-//     break;
-//   case "km":
-//     valueMeters / 1000;
-//     alert("O valor é: " + valueMeters / 1000 + " quilometros");
-//     break;
-//   default:
-//     alert("Valor de medida incorreto");
-// }
+// Resultado fazendo cálculo direto na variável
 
 switch (measurementChoice) {
-  case "mm":
-    resultChoice = valueMeters * 1000;
-    break
-  case "cm":
-    resultChoice = valueMeters * 100;
-    break
-  case "dm":
-    resultChoice = valueMeters * 10;
-    break
-  case "dam":
-    resultChoice = valueMeters / 10;
-    break
-  case "hm":
-    resultChoice = valueMeters / 100;
-    break
-  case "km":
-    resultChoice = valueMeters / 1000;
-    break
+  case "1":
+    valueMeters * 1000;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters * 1000 + " milimetros");
+    break;
+  case "2":
+    valueMeters * 100;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters * 100 + " centímetros");
+    break;
+  case "3":
+    valueMeters * 10;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters * 10 + " decímetros");
+    break;
+  case "4":
+    valueMeters / 10;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters / 10 + " decâmetros");
+    break;
+  case "5":
+    valueMeters / 100;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters / 100 + " hectômetros");
+    break;
+  case "6":
+    valueMeters / 1000;
+    alert("Resultado é: " + valueMeters + "m = " + valueMeters / 1000 + " quilometros");
+    break;
   default:
-    alert("Formato de medida errado");
+    alert("Valor de medida incorreto");
 }
 
-alert(resultChoice + " milimetros");
+// Resultado fazendo cálculo através de uma variável alternativa
+
+// switch (measurementChoice) {
+//   case "mm":
+//     resultChoice = valueMeters * 1000;
+//     break
+//   case "cm":
+//     resultChoice = valueMeters * 100;
+//     break
+//   case "dm":
+//     resultChoice = valueMeters * 10;
+//     break
+//   case "dam":
+//     resultChoice = valueMeters / 10;
+//     break
+//   case "hm":
+//     resultChoice = valueMeters / 100;
+//     break
+//   case "km":
+//     resultChoice = valueMeters / 1000;
+//     break
+//   default:
+//     alert("Formato de medida errado");
+// }
+
+// alert(resultChoice + " milimetros");
